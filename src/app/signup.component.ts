8288fafd88759391c6ae87b05d0af31eb03d1ec6
217205135 <217205135@mycput.ac.za>
2021-09-14 23:06:48 +0200

    217205135

--- src/app/signup.component.ts
+++ src/app/signup.component.ts
@@ -1,5 +1,16 @@
+import {NgModule} from '@angular/core';
 import { Component } from '@angular/core';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatFormFieldModule, MAT_FORM_FIELD_DEFAULT_OPTIONS} from '@angular/material/form-field';
 
+@NgModule({
+imports:[
+  MatButtonModule,
+    MatFormFieldModule,
+    MatGridListModule
+]
+})
 @Component({
   selector: 'app-root',
   templateUrl: './signup.component.html',
