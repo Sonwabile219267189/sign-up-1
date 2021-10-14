3d84f167ae8c5d35485a14c22a454715e6a10a66
217205135 <217205135@mycput.ac.za>
2021-10-14 14:14:59 +0200

    217205135

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { AboutComponent } from './about.component';
 import { LoginComponent } from './login.component';
 import { SignupComponent} from './signup.component';
 import { CreateBusnissComponent} from './create-busniss.component';
+import { SurfBusinessComponent } from './surf-business.component';
 
 
 const routes: Routes =[
   {path:'login',component: LoginComponent},
-  {path:'',component:AboutComponent},
+  {path:'about',component:AboutComponent},
   {path:'sighnup',component:SignupComponent},
-  {path:'create-busniss',component:CreateBusnissComponent}
+  {path:'create-busniss',component:CreateBusnissComponent},
+  {path: 'surf-business',component:SurfBusinessComponent}
   
 ];
 
